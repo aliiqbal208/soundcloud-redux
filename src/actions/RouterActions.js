d68daa6ae26377d7fb5302c5be31f2c911ef2ddf
aliiqbal208 <aliiqbal208@gmail.com>
2020-10-20 06:52:19 -0700

    First commit

---
+++ src/actions/RouterActions.js
@@ -0,0 +1,44 @@
+/* global history */
+/* global location */
+/* global window */
+import { CHANGE_ROUTE } from '../constants/ActionTypes';
+import { compileHash, parseRoute } from '../utils/RouterUtils';
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
+
+const pushState = (route) => {
+  const hash = compileHash(route);
+  debugger
+  if (window.location.hash !== hash) {
+    history.push({ route }, '', hash);
+  }
+};
+
+export const navigateTo = (route, shouldPushState = true) => {
+  if (shouldPushState) {
+    pushState(route);
+  }
+
+  return {
+    type: CHANGE_ROUTE,
+    route,
+  };
+};
+
+export const navigateBack = e => (dispatch) => {
+  const { state } = e;
+  if (state) {
+    const { route } = state;
+    dispatch(navigateTo(route, false));
+  }
+};
+
+export const initRouter = paths => (dispatch) => {
+  window.onpopstate = (e) => {
+    dispatch(navigateBack(e));
+  };
+
+  const hash = window.location.hash ? window.location.hash.slice(2) : '';
+  const route = parseRoute(hash, paths);
+  return dispatch(navigateTo(route));
+};
